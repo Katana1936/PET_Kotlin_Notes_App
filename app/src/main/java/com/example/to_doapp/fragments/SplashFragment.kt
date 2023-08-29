6f870d712b3aa488b9b3641c3b45f3a1fcb60e10
Karina Zubko <karinazubko2017@gmail.com>
2023-08-29 18:27:08 +0300

    Added animation for splash window

--- app/src/main/java/com/example/to_doapp/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/to_doapp/fragments/SplashFragment.kt
@@ -6,9 +6,11 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.example.to_doapp.R
 import com.google.firebase.auth.FirebaseAuth
 
@@ -20,11 +22,20 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init(view)
+
+        val gifImageView: ImageView = view.findViewById(R.id.gifImageView)
+
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.splash_cat)
+            .into(gifImageView)
+
         val isLogin: Boolean = mAuth.currentUser != null
         val handler = Handler(Looper.myLooper()!!)
         handler.postDelayed({
@@ -32,9 +43,9 @@ class SplashFragment : Fragment() {
                 navController.navigate(R.id.action_splashFragment_to_homeFragment)
             else
                 navController.navigate(R.id.action_splashFragment_to_signInFragment)
-
         }, 2000)
     }
+
     private fun init(view: View) {
         mAuth = FirebaseAuth.getInstance()
         navController = Navigation.findNavController(view)
