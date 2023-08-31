81188d59647e936fb7dc2805174d87ff9379694b
Karina Zubko <karinazubko2017@gmail.com>
2023-08-31 23:09:58 +0300

    Problems with cutting and hiding panels have been worked out

--- app/src/main/java/com/example/to_doapp/MainActivity.kt
+++ app/src/main/java/com/example/to_doapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.to_doapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        window.insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        window.setDecorFitsSystemWindows(false)
     }
 }
