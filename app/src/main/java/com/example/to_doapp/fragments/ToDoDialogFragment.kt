b68f7978a7cddf0e9a7c7b43360f7f8f6a0965b8
Karina Zubko <karinazubko2017@gmail.com>
2023-09-04 12:17:51 +0300

    Adding an element for a fragment to add a task

--- app/src/main/java/com/example/to_doapp/fragments/ToDoDialogFragment.kt
+++ app/src/main/java/com/example/to_doapp/fragments/ToDoDialogFragment.kt
@@ -1,5 +1,4 @@
 package com.example.to_doapp.fragments
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,19 +7,13 @@ import androidx.fragment.app.DialogFragment
 import com.example.to_doapp.databinding.FragmentToDoDialogBinding
 import com.example.to_doapp.utils.model.ToDoData
 import com.google.android.material.textfield.TextInputEditText
-
-
 class ToDoDialogFragment : DialogFragment() {
-
     private lateinit var binding: FragmentToDoDialogBinding
     private var listener : OnDialogNextBtnClickListener? = null
     private var toDoData: ToDoData? = null
-
-
     fun setListener(listener: OnDialogNextBtnClickListener) {
         this.listener = listener
     }
-
     companion object {
         const val TAG = "DialogFragment"
         @JvmStatic
@@ -32,19 +25,13 @@ class ToDoDialogFragment : DialogFragment() {
                 }
             }
     }
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         binding = FragmentToDoDialogBinding.inflate(inflater , container,false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (arguments != null){
@@ -67,7 +54,6 @@ class ToDoDialogFragment : DialogFragment() {
             }
         }
     }
-
     interface OnDialogNextBtnClickListener{
         fun saveTask(todoTask:String , todoEdit:TextInputEditText)
         fun updateTask(toDoData: ToDoData , todoEdit:TextInputEditText)
